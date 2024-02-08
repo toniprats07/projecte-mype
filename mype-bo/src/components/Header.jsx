50b1487b8ca4e52a1fc434eb577c98cadb8521d4
toniprats07 <119038749+toniprats07@users.noreply.github.com>
2024-02-08 17:31:48 +0100

    Cambios en responsive como el menú horizontal

--- mype-bo/src/components/Header.jsx
+++ mype-bo/src/components/Header.jsx
@@ -3,60 +3,50 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  // Función para alternar entre abrir y cerrar el menú
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="flex items-center justify-between p-4 header text-color">
-      <div className="navbar">
-        <div className="navbar-start">
-          <a href='/'>
-            <img src='https://www.mype.com/wp-content/uploads/2024/01/Logo-MYPE.png' alt='Mype Logo' className='h-12'></img>
-          </a>
-        </div>
-        <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 text-xl">
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/nosotros">Sobre Mype</Link></li>
-            <li><Link to="/servicios">Servicios</Link></li>
-            <li><Link to="/proyectos">Proyectos</Link></li>
-            <li><Link to="/catalogo">Catálogo</Link></li>
-            <li><Link to="/contacto">Contacto</Link></li>
-          </ul>
-        </div>
+    <header className="p-4 flex justify-between items-center">
+      <a href="/" className="flex items-center">
+        <img src="https://www.mype.com/wp-content/uploads/2024/01/Logo-MYPE.png" alt="Mype Logo" className="h-12" />
+      </a>
 
-        {/* Botón de menú/desplegable */}
-        <div className="dropdown lg:hidden">
-          <div tabIndex={0} role="button" className="btn btn-ghost ml-40" onClick={toggleMenu}>
-            {menuOpen ? (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-              </svg>
-            )}
-          </div>
-          
-          {/* Menú desplegable */}
-          <ul
-            tabIndex={0} className={`menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow box-color rounded-box w-full text-2xl ${menuOpen ? 'visible' : 'hidden'}`}>
-            <li><Link to="/home" onClick={toggleMenu}>Inicio</Link></li>
-            <li><Link to="/nosotros" onClick={toggleMenu}>Sobre Mype</Link></li>
-            <li><Link to="/servicios" onClick={toggleMenu}>Servicios</Link></li>
-            <li><Link to="/proyectos" onClick={toggleMenu}>Proyectos</Link></li>
-            <li><Link to="/catalogo" onClick={toggleMenu}>Catálogo</Link></li>
-            <li><Link to="/contacto" onClick={toggleMenu}>Contacto</Link></li>
-          </ul>
-        </div>
-      </div>
+      {/* Botón de menú hamburguesa para móviles */}
+      <button onClick={() => setIsOpen(!isOpen)} className="lg:hidden">
+        <svg className="w-20 h-20" fill="none" stroke="white" viewBox="0 0 24 24">
+          {isOpen ? (
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          ) : (
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+          )}
+        </svg>
+      </button>
+
+      {/* Menú de navegación */}
+      <nav className={`lg:flex ${isOpen ? 'flex' : 'hidden'} z-[1] flex-col lg:flex-row absolute lg:relative w-full lg:w-auto color-menu lg:bg-transparent left-0 lg:left-auto top-24 lg:top-auto transition-top duration-300 ease-in-out`}>
+        <ul className="flex flex-col lg:flex-row items-center lg:items-stretch">
+          <li className="my-4 lg:my-2 lg:mx-4">
+            <a href="/" className="text-color sm:text-5xl lg:text-xl hover:text-yellow-500">Inicio</a>
+          </li>
+          <li className="my-4 lg:my-2 lg:mx-4">
+            <a href="/nosotros" className="text-color sm:text-5xl lg:text-xl hover:text-yellow-500">Sobre Mype</a>
+          </li>
+          <li className="my-4 lg:my-2 lg:mx-4">
+            <a href="/servicios" className="text-color sm:text-5xl lg:text-xl hover:text-yellow-500">Servicios</a>
+          </li>
+          <li className="my-4 lg:my-2 lg:mx-4">
+            <a href="/proyectos" className="text-color sm:text-5xl lg:text-xl hover:text-yellow-500">Proyectos</a>
+          </li>
+          <li className="my-4 lg:my-2 lg:mx-4">
+            <a href="/catalogo" className="text-color sm:text-5xl lg:text-xl hover:text-yellow-500">Catálogo</a>
+          </li>
+          <li className="my-4 lg:my-2 lg:mx-4">
+            <a href="/contacto" className="text-color sm:text-5xl lg:text-xl hover:text-yellow-500">Contacto</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
-}
+};
 
 export default Header;
